f5a04d704cbc505384e04ab3add5af2402ce61c8
Josh <joshbergstrom463@gmail.com>
2020-04-28 14:35:58 -0700

    Setup
    Added Grappling scripits and set up a small scene

---
+++ Assets/Scripits/GrappingHook.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappingHook : MonoBehaviour
+{
+    public GameObject hook;
+    public GameObject hookHolder;
+
+    public float hookTravelSpeed;
+    public float playerTravelSpeed;
+
+    public static bool fired;
+    public bool hooked;
+
+    public float maxDistance;
+    private float currentDistance;
+
+    private void Update()
+    {
+        //Firing the hook
+        if (Input.GetKeyDown(KeyCode.E) && fired == false)
+        {
+            fired = true;
+        }
+
+        if (fired == true && hooked == false)
+        {
+            hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
+            currentDistance = Vector3.Distance(transform.position, hook.transform.position);
+
+            if (currentDistance >= maxDistance)
+            {
+                returnHook();
+            }
+        }
+
+        if (hooked == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hook.transform.position, Time.deltaTime * playerTravelSpeed);
+            float distanceToHook = Vector3.Distance(transform.position, hook.transform.position);
+
+            //this.GetComponent<Rigidbody>().useGravity = false;
+
+
+            if (distanceToHook < 1)
+            {
+                returnHook();
+            }
+            else
+            {
+                //this.GetComponent<Rigidbody>().useGravity = true;
+                
+            }
+        }
+
+    }
+
+    void returnHook()
+    {
+        hook.transform.position = hookHolder.transform.position;
+        fired = false;
+        hooked = false;
+    }
+}
---
+++ Assets/Scripits/HookDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookDetector : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hookable")
+        {
+            player.GetComponent<GrappingHook>().hooked = true;
+        }
+    }
+
+}
